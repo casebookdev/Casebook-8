2a990149a2cd9c9bb393520a26308c328182c877
Marcus Bloice <marcus.bloice@medunigraz.at>
2014-07-31 13:34:51 +0200

    Interface and Swift Tests (Core Data Tests)

--- Casebook 8/FirstViewController.swift
+++ Casebook 8/FirstViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-                            
+    
+    
+    // IB Outlets are just created here now instead of the
+    @IBOutlet var txtCaseName: UITextField
+    @IBOutlet var txtAccessCode: UITextField
+    
+    // IB Actions also appear here in Swift
+    @IBAction func btnSave(){
+        txtCaseName.text = "Save Button Pressed"
+    }
+    
+    @IBAction func btnLoad(){
+        txtCaseName.text = "Load Button Pressed"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
