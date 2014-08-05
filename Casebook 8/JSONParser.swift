024a4467d17e4cedfa0ef807dc3b3784bb715e38
Marcus Bloice <marcus.bloice@medunigraz.at>
2014-08-05 11:20:39 +0200

    Beginning translation of JSON parse code to Swift.
    See comments in JSONParser.swift file.

---
+++ Casebook 8/JSONParser.swift
@@ -0,0 +1,18 @@
+//
+//  JSONParser.swift
+//  Casebook 8
+//
+//  Created by Marcus Bloice on 05/08/14.
+//  Copyright (c) 2014 Marcus D. Bloice. All rights reserved.
+//
+
+import Foundation
+
+// Here we shall import the parsing code from the previous project and convert it into Swift code. 
+// As an example JSON file, will parse the file that was uploaded with the BMC article.
+// Note: Inlclude reference to Casebook article here.
+
+// JSON (JMCR.json) that shall be parsed here for testing is from:
+// Ammannagari N, Chikoti S, Bravin E: Hodgkin's lymphoma presenting as a complex paraneoplastic 
+// neurological syndrome: a case report. Journal of Medical Case Reports 2013, 7:96.
+
